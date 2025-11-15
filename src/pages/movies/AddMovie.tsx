2d91d3a9843ea41959aa4a55b4332616bed975b7
ernestwijaya <dheren90@gmail.com>
2025-11-15 18:30:55 +0700

    Day 2 Belajar React

--- src/pages/movies/AddMovie.tsx
+++ src/pages/movies/AddMovie.tsx
@@ -3,7 +3,7 @@ import { NavLink } from "react-router";
 function AddMovie(){
     return <div>
         <h2>Add Movie Page</h2>
-        <NavLink to = "/" className = "btn btn-primary">Back to Movies</NavLink>
+        <NavLink to = "/" className = "btn btn-primary">List Movies</NavLink>
         
     </div>
 }
