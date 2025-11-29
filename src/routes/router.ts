642546352e7f20c1b6e715359adc07650b9af5c6
Andreyas3667 <dheren90@gmail.com>
2025-11-29 18:41:43 +0700

    Day Kesekian Belajar React

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -1,29 +1,43 @@
 import { createBrowserRouter } from "react-router";
 
 const router = createBrowserRouter([
-    {
-        path : "/",
-        children : [
-            {
-                index :true,
-                lazy: {
-                    Component : async () => {
-                        const component = await import("../pages/movies/Movies.tsx")
-                        return component.default
-                    }
-                }
-            },
-            {
-                path : "add-movie",
-                lazy: {
-                    Component : async () => {
-                        const component = await import("../pages/movies/AddMovie.tsx")
-                        return component.default
-                    }
-                }
-            }
-        ]
-    }
-])
+  {
+    path: "/",
+    children: [
+      {
+        index: true,
+        lazy: {
+          Component: async () => {
+            const component = await import("../pages/movies/auth/signup/SignUp.tsx");
+            return component.default;
+          }
+        }
+      },
+    ]
+  },
+  {
+    path: "/movies",
+    children: [
+      {
+        index: true,
+        lazy: {
+          Component: async () => {
+            const component = await import("../pages/movies/Movies.tsx");
+            return component.default;
+          }
+        }
+      },
+      {
+        path: "add-movie",
+        lazy: {
+          Component: async () => {
+            const component = await import("../pages/movies/AddMovie.tsx");
+            return component.default;
+          }
+        }
+      }
+    ]
+  }
+]);
 
-  export default router;
+export default router;
